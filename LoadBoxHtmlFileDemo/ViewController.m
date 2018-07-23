99e6dcc9d290b39554c5ba747b8b76fc881f6994
luck <likangios@163.com>
2018-07-23 08:36:57 +0800

    no message

---
+++ LoadBoxHtmlFileDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  LoadBoxHtmlFileDemo
+//
+//  Created by luck on 2018/7/23.
+//  Copyright © 2018年 luck. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
